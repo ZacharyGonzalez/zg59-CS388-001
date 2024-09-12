b78da60d435690f278539e7d14f8a937fbaee3c4
Zachary <Zg59@njit.edu>
2024-09-12 14:14:31 -0400

    adding prework to repo

---
+++ Prework/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.helloworld
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            findViewById<Button>(R.id.b1).setOnClickListener{ _->
+                Toast.makeText(applicationContext, "HELLO TO YOU TOO!", Toast.LENGTH_SHORT).show()
+            }
+
+            insets
+        }
+    }
+}
